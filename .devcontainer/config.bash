f826a8c3c956f3894a69cd5ec064d78ab29affbe
Harco Kuppens <h.kuppens@gmail.com>
2024-06-03 22:42:24 +0200

    rdp port dockert host on port 6389 instead of 3389

--- .devcontainer/config.bash
+++ .devcontainer/config.bash
@@ -3,7 +3,7 @@ export CONFIG_USER="robot"
 export CONFIG_PASSWORD="maker"
 export CONFIG_IMAGE="img-ev3dev2robot"
 export CONFIG_CONTAINER="ev3dev2robot"
-export CONFIG_HOSTPORT_RDP="3389"
+export CONFIG_HOSTPORT_RDP="6389"
 export CONFIG_HOSTPORT_EV3DEV2="6840"
 export CONFIG_HOSTPORT_BLUETOOTH="6841"
 
